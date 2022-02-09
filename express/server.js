3e72bc6e11ac0ff86d14364e5ae18cf7b8d44212
temhbin <temhbin@bk.ru>
2022-02-08 17:18:43 -0800

    update backend

--- express/server.js
+++ express/server.js
@@ -8,11 +8,14 @@ const bodyParser = require('body-parser');
 const router = express.Router();
 router.get('/', (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
+  res.write('<h1>Hello from Express.js!fff</h1>');
   res.end();
 });
-router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-router.post('/', (req, res) => res.json({ postBody: req.body }));
+app.get('/another', (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write('<h1>Hello from Express.sdfsfsdfsdfsd!fff</h1>');
+  res.end();
+});
 
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
