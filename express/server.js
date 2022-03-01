9d1195001f32dab8452eb77570bce5a394558ee1
Denis Balandin <denisbalandin@Deniss-MacBook-Pro.local>
2022-02-28 19:12:01 -0800

    update for backend for getting data from DB

--- express/server.js
+++ express/server.js
@@ -1,50 +1,44 @@
-'use strict';
-const express = require('express');
-const path = require('path');
-const serverless = require('serverless-http');
+"use strict";
+
+const express = require("express");
+const config = require("../config.json");
+const path = require("path");
+const serverless = require("serverless-http");
 const app = express();
-const bodyParser = require('body-parser');
-const mysql = require('mysql');
+const bodyParser = require("body-parser");
+const mysql = require("mysql");
+const cors = require("cors");
 
-const pool = mysql.createPool({
-  host     : 'host',
-  user     : 'user',
-  password : 'pass',
-  database : 'db'
-});
-
-
+app.use(cors());
+const pool = mysql.createPool(config);
 const router = express.Router();
-router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
+
+router.get("/", (req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.write("<h1>Hello from Express.js!</h1>");
   res.end();
 });
 
-app.get('/user-list', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.jsffff!</h1>');
+app.get("/user-list", (req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.write("<h1>Hello from Express.jsffff!</h1>");
   res.end();
 });
-
-app.get("/example",(req,res) => {
+app.get("/example", (req, res) => {
   pool.getConnection((err, connection) => {
-      if(err) throw err;
-      connection.query('SELECT * from np_news', (err, rows) => {
-          connection.release(); // return the connection to pool
-          if(err) throw err;
-          res.send({ data: rows });
-          console.log('The data from users table are: \n', rows);
-      });
+    if (err) throw err;
+    connection.query("SELECT * from np_news", (err, rows) => {
+      connection.release(); // return the connection to pool
+      if (err) throw err;
+      res.send({ data: rows });
+      //  console.log("The data from users table are: \n", rows);
+    });
   });
 });
 
-
-
-
 app.use(bodyParser.json());
-app.use('/.netlify/functions/server', router);  // path must route to lambda
-app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+app.use("/.netlify/functions/server", router); // path must route to lambda
+app.use("/", (req, res) => res.sendFile(path.join(__dirname, "../index.html")));
 
 module.exports = app;
 module.exports.handler = serverless(app);
