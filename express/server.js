850ee61039ae62ddc303f9d0a026bf436c79271f
gareginra <40554131+gareginra@users.noreply.github.com>
2022-04-11 05:50:25 +0400

    mysql-base

--- express/server.js
+++ express/server.js
@@ -28,28 +28,30 @@ app.get("/news", (req, res) => {
   pool.getConnection((err, connection) => {
     if (err) throw err;
     connection.query(
-      "SELECT wf02_page.id, wf02_page.slug, wf02_page.title, wf02_page.created_on, wf02_page_part.content, wf02_page_part.name from wf02_page INNER JOIN wf02_page_part ON wf02_page.id = wf02_page_part.page_id WHERE wf02_page.parent_id = 11 ",
+      // "SELECT wf02_page.id, wf02_page.slug, wf02_page.title, wf02_page.created_on, wf02_page_part.content, wf02_page_part.name from wf02_page INNER JOIN wf02_page_part ON wf02_page.id = wf02_page_part.page_id WHERE wf02_page.parent_id = 11 ",
+      "SELECT * FROM np_news",
       (err, response) => {
         connection.release(); // return the connection to pool
         if (err) throw err;
-        let ceshMap = {};
-        let description = "";
-        let text = "";
-        const newsBlog = [];
-        for (let val of response) {
-          const { id, title, slug, name, content, created_on: createdAt } = val;
-          if (name === "body") {
-            text = content;
-          }
-          if (name === "description") {
-            description = content;
-          }
-          ceshMap[id] = { id, title, slug, text, description, createdAt };
-        }
-        for (let news in ceshMap) {
-          newsBlog.push(ceshMap[news]);
-        }
-        res.send({ data: newsBlog });
+        // let ceshMap = {};
+        // let description = "";
+        // let text = "";
+        // const newsBlog = [];
+        // console.log(response)
+        // for (let val of response) {
+        //   const { id, title, slug, name, content, created_on: createdAt } = val;
+        //   if (name === "body") {
+        //     text = content;
+        //   }
+        //   if (name === "description") {
+        //     description = content;
+        //   }
+        //   ceshMap[id] = { id, title, slug, text, description, createdAt };
+        // }
+        // for (let news in ceshMap) {
+        //   newsBlog.push(ceshMap[news]);
+        // }
+        res.send(response);
       }
     );
   });
@@ -57,55 +59,104 @@ app.get("/news", (req, res) => {
 app.get("/blog", (req, res) => {
   pool.getConnection((err, connection) => {
     if (err) throw err;
-    connection.query(
-      "SELECT wf02_page.id, wf02_page.slug, wf02_page.title, wf02_page.created_on, wf02_page_part.content, wf02_page_part.name from wf02_page INNER JOIN wf02_page_part ON wf02_page.id = wf02_page_part.page_id WHERE wf02_page.parent_id = 24 ",
-      (err, response) => {
-        connection.release(); // return the connection to pool
-        if (err) throw err;
-        let ceshMap = {};
-        let description = "";
-        let text = "";
-        let image = "";
-        let category = "";
-        let views = "";
-        const newsBlog = [];
-        for (let val of response) {
-          const { id, title, slug, name, content, created_on: createdAt } = val;
-          if (name === "body") {
-            text = content;
-          }
-          if (name === "description") {
-            description = content;
-          }
-          if (name === "image") {
-            image = content;
-          }
-          if (name === "category") {
-            category = content;
-          }
-          if (name === "views") {
-            views = content;
-          }
-          ceshMap[id] = {
-            id,
-            title,
-            slug,
-            text,
-            description,
-            createdAt,
-            image,
-            category,
-            views,
-          };
-        }
-        for (let news in ceshMap) {
-          newsBlog.push(ceshMap[news]);
-        }
-        res.send({ data: newsBlog });
-      }
-    );
-  });
-});
+    connection.query('SELECT * FROM np_blog', (err, response) => {
+      connection.release(); // return the connection to pool
+      if (err) throw err;
+      // let ceshMap = {};
+      // let description = "";
+      // let text = "";
+      // let image = "";
+      // let category = "";
+      // let views = "";
+      // const newsBlog = [];
+      // for (let val of response) {
+      //   const { id, title, slug, name, content, created_on: createdAt } = val;
+      //   if (name === "body") {
+      //     text = content;
+      //   }
+      //   if (name === "description") {
+      //     description = content;
+      //   }
+      //   if (name === "image") {
+      //     image = content;
+      //   }
+      //   if (name === "category") {
+      //     category = content;
+      //   }
+      //   if (name === "views") {
+      //     views = content;
+      //   }
+      //   ceshMap[id] = {
+      //     id,
+      //     title,
+      //     slug,
+      //     text,
+      //     description,
+      //     createdAt,
+      //     image,
+      //     category,
+      //     views,
+      //   };
+      // }
+      // for (let news in ceshMap) {
+      //   newsBlog.push(ceshMap[news]);
+      // }
+      res.send(response);
+    });
+  })
+})
+// app.get("/blog", (req, res) => {
+//   pool.getConnection((err, connection) => {
+//     if (err) throw err;
+//     connection.query(
+//       "SELECT wf02_page.id, wf02_page.slug, wf02_page.title, wf02_page.created_on, wf02_page_part.content, wf02_page_part.name from wf02_page INNER JOIN wf02_page_part ON wf02_page.id = wf02_page_part.page_id WHERE wf02_page.parent_id = 24 ",
+//       (err, response) => {
+//         connection.release(); // return the connection to pool
+//         if (err) throw err;
+//         let ceshMap = {};
+//         let description = "";
+//         let text = "";
+//         let image = "";
+//         let category = "";
+//         let views = "";
+//         const newsBlog = [];
+//         for (let val of response) {
+//           const { id, title, slug, name, content, created_on: createdAt } = val;
+//           if (name === "body") {
+//             text = content;
+//           }
+//           if (name === "description") {
+//             description = content;
+//           }
+//           if (name === "image") {
+//             image = content;
+//           }
+//           if (name === "category") {
+//             category = content;
+//           }
+//           if (name === "views") {
+//             views = content;
+//           }
+//           ceshMap[id] = {
+//             id,
+//             title,
+//             slug,
+//             text,
+//             description,
+//             createdAt,
+//             image,
+//             category,
+//             views,
+//           };
+//         }
+//         for (let news in ceshMap) {
+//           newsBlog.push(ceshMap[news]);
+//         }
+//         res.send({ data: newsBlog });
+//       }
+//     );
+//   });
+// });
 
 app.use(bodyParser.json());
 app.use("/.netlify/functions/server", router); // path must route to lambda
